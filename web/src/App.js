5260836e0a15f8567c1e32849d00c901027f45f6
Alan Morais <alan.franc@hotmail.com>
2020-01-31 05:30:57 -0300

    [finalizando]

--- web/src/App.js
+++ web/src/App.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
-
+import api from './services/api';
 import './global.css';
 import './App.css';
 import './Sidebar.css';
 import './Main.css';
 
 function App() {
-  const [latitue, setLatitue] = useState('');
+  const [devs, setDevs] = useState([]);
+  const [latitude, setLatitue] = useState('');
   const [longitude, setLongitude] = useState('');
   const [github_username, setGithub_username] = useState('');
   const [techs, setTechs] = useState('');
@@ -28,108 +29,85 @@ function App() {
     )
   }, []);
 
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('./devs')
+      setDevs(response.data);
+    }
+    loadDevs();
+  }, []);
+
+
   async function handleAddDev(e) {
     e.preventDefalt();
-const response = await api.post('/devs', {
-  github_username,
-  techs,
-  latitude,
-  longitude,
-})
+    const response = await api.post('/devs', {
+      github_username,
+      techs,
+      latitude,
+      longitude,
+    })
 
-setGithub_username('');
-setTechs('');
+    setGithub_username('');
+    setTechs('');
 
-  return (
-    <div id="app">
-      <aside>
-        <strong>Cadrastrar</strong>
-        <form>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="github_username" required
-              value={github_username}
-              onChange={e => setGithub_username(e.target.value)} />
-          </div>
+    setDevs([...devs, response.data]);
 
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required value={techs}
-              onChange={e => setTechs(e.target.value)} />
-          </div>
-
-          <div className="input-group">
+    return (
+      <div id="app">
+        <aside>
+          <strong>Cadrastrar</strong>
+          <form>
             <div className="input-block">
-              <label htmlFor="latitude">latitude</label>
-              <input type="number" name="latitude" id="latitude" required value={latitue}
-                onChange={e => setLatitue(e.target.value)} />
+              <label htmlFor="github_username">Usuário do Github</label>
+              <input name="github_username" id="github_username" required
+                value={github_username}
+                onChange={e => setGithub_username(e.target.value)} />
             </div>
 
             <div className="input-block">
-              <label htmlFor="longitude">longitude</label>
-              <input type="number" name="longitude" id="longitude" required value={longitude}
-                onChange={e => setLongitude(e.target.value)} />
+              <label htmlFor="techs">Tecnologias</label>
+              <input name="techs" id="techs" required value={techs}
+                onChange={e => setTechs(e.target.value)} />
             </div>
-          </div>
-          <button type="submit">Salvar</button>
-        </form>
-      </aside>
 
-      <main>
-        <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/27930049?s=460&v=4" alt="Alan França" />
-              <div className="user-info">
-                <strong>Alan França</strong>
-                <span>HTML, CSS, Javascript, Python</span>
+            <div className="input-group">
+              <div className="input-block">
+                <label htmlFor="latitude">latitude</label>
+                <input type="number" name="latitude" id="latitude" required value={latitude}
+                  onChange={e => setLatitue(e.target.value)} />
               </div>
-            </header>
-            <p>Cientista da Computação e sempre estudando</p>
-            <a href="https://github.com/alan-morais">Acessar perfil no Github</a>
-          </li>
 
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/27930049?s=460&v=4" alt="Alan França" />
-              <div className="user-info">
-                <strong>Alan França</strong>
-                <span>HTML, CSS, Javascript, Python</span>
+              <div className="input-block">
+                <label htmlFor="longitude">longitude</label>
+                <input type="number" name="longitude" id="longitude" required value={longitude}
+                  onChange={e => setLongitude(e.target.value)} />
               </div>
-            </header>
-            <p>Cientista da Computação e sempre estudando</p>
-            <a href="https://github.com/alan-morais">Acessar perfil no Github</a>
-          </li>
+            </div>
+            <button type="submit">Salvar</button>
+          </form>
+        </aside>
 
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/27930049?s=460&v=4" alt="Alan França" />
-              <div className="user-info">
-                <strong>Alan França</strong>
-                <span>HTML, CSS, Javascript, Python</span>
-              </div>
-            </header>
-            <p>Cientista da Computação e sempre estudando</p>
-            <a href="https://github.com/alan-morais">Acessar perfil no Github</a>
-          </li>
+        <main>
+          <ul>
+            {devs.map(dev => (
+              <li key={dev._id} className="dev-item">
+                <header>
+                  <img src={dev.avatar_url} alt={dev.name} />
+                  <div className="user-info">
+                    <strong>{dev.name}</strong>
+                    <span>{dev.techs.join(', ')}</span>
+                  </div>
+                </header>
+                <p>{dev.bio}</p>
+                <a href={`https://github.com/${dev.github_username}`}>Acessar perfil no Github</a>
+              </li>
+            ))}
+          </ul>
+        </main>
 
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/27930049?s=460&v=4" alt="Alan França" />
-              <div className="user-info">
-                <strong>Alan França</strong>
-                <span>HTML, CSS, Javascript, Python</span>
-              </div>
-            </header>
-            <p>Cientista da Computação e sempre estudando</p>
-            <a href="https://github.com/alan-morais">Acessar perfil no Github</a>
-          </li>
-        </ul>
-      </main>
-
-    </div>
-  );
-}
+      </div>
+    );
+  }
 }
 
 export default App;
